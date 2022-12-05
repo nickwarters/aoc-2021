489d0327edd9a9f56a57e4598024a53664cf400c
Nick Warters <nick.warters@gmail.com>
2022-12-05 19:29:11 +0000

    add template

---
+++ src/00/part1.ts
@@ -0,0 +1,21 @@
+import { readFileSync } from 'fs'
+
+function solve(input: string): any {
+	throw new Error('function not implemented')
+}
+
+const tests: [string, any][] = [[``, 0]]
+
+tests.forEach(([testData, expected]) => {
+	const result = solve(testData)
+	console.log(
+		`Example Input Solution - Expected: ${expected}, Got: ${result}, ${
+			result === expected ? 'PASS' : 'FAIL'
+		}`
+	)
+})
+
+console.log(
+	'Full Input Solution',
+	solve(readFileSync('./input.txt', { encoding: 'utf-8' }))
+)
